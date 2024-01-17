e4c0eeabe6a28cdf8307f578c3cf45cc12e88cda
Emma Casey <22841956+fiffy326@users.noreply.github.com>
2024-01-17 23:39:45 +1000

    [PATCH] dwm-default-tag-apps-20210327-61bb8b2.diff

--- config.def.h
+++ config.def.h
@@ -50,6 +50,7 @@ static const unsigned int alphas[][3] = {
 static const int momentaryalttags = 0; /* 1 means alttags will show only when key is held down*/
 static const char *tags[]    = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const char *tagsalt[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *defaulttagapps[] = { "st", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL };
 
 static unsigned int ulinepad	= 5;	/* horizontal padding between the underline and tag */
 static unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
@@ -161,6 +162,7 @@ static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_s,      spawndefault,   {0} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
