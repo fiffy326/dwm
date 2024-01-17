3f634c29772780127416b747a6b25e159be2dbcd
Emma Casey <22841956+fiffy326@users.noreply.github.com>
2024-01-18 00:51:39 +1000

    Added option to show/hide window title in bar

--- dwm.c
+++ dwm.c
@@ -893,7 +893,7 @@ drawbar(Monitor *m)
 	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
 
 	if ((w = m->ww - tw - x) > bh) {
-		if (m->sel) {
+		if (m->sel && showtitle) {
 			drw_setscheme(drw, scheme[m == selmon ? SchemeTitle : SchemeNorm]);
 			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
 			if (m->sel->isfloating)
